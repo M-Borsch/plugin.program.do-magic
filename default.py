a8434fa436b7779abb6a0b9270999a22b65a8dcf
Mike Borsch <154018836+M-Borsch@users.noreply.github.com>
2026-04-25 21:21:00 -0400

    Refactor Kodi Favourites add-on for Python 3 compatibility

--- default.py
+++ default.py
@@ -5,7 +5,7 @@
 # favourites.xml file.
 #
 # --------------------------------------------------------------------
-# M-Borsch 2026-02-28: Version 1.0.0
+# M-Borsch 2026-02-28: Version 1.0
 # - Initial Release
 # --------------------------------------------------------------------
 #
@@ -70,7 +70,7 @@ def execDownloadFunction():
 
 def download_with_progress(url, dest_name):
     dp = xbmcgui.DialogProgress()
-    dp.create('Downloading', 'Starting download...')
+    dp.create('Downloading', 'Starting download...\n\nPlease Wait...')
     
     # Use xbmcvfs to handle paths across different OSs
     save_path = xbmcvfs.translatePath(os.path.join(magicDir, dest_name))
